b001b4e1dc817971815fb80e97dd9935d93abb1c
morte <202109169@post.au.dk>
2026-04-21 15:07:47 +0200

    setup file

--- dongle/setup_lowpan.sh
+++ dongle/setup_lowpan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-ID=$1
-
 ip link set wpan0 up
 ip link add link wpan0 name lowpan0 type lowpan
 ip link set lowpan0 up
@@ -10,7 +8,7 @@ iwpan phy phy0 set channel 0 24
 ip link set lowpan0 down
 ip link set wpan0 down
 iwpan dev wpan0 set ackreq_default 1
-iwpan dev wpan0 set short_addr 0x000$ID
+iwpan dev wpan0 set short_addr 0x0004
 iwpan dev wpan0 set pan_id 0x04D2
 ip link set wpan0 up
 ip link set lowpan0 up
@@ -18,4 +16,25 @@ ip link set lowpan0 up
 sysctl -w net.ipv6.conf.lowpan0.forwarding=1
 sysctl -w net.ipv6.conf.lowpan0.accept_ra=1
 
-ip -6 addr add fd00::$ID/64 dev lowpan0
+ip -6 addr add fd00::4/64 dev lowpan0
+# #!/bin/bash
+
+# ID=$1
+
+# ip link set wpan0 up
+# ip link add link wpan0 name lowpan0 type lowpan
+# ip link set lowpan0 up
+
+# iwpan phy phy0 set channel 0 24
+# ip link set lowpan0 down
+# ip link set wpan0 down
+# iwpan dev wpan0 set ackreq_default 1
+# iwpan dev wpan0 set short_addr 0x000$ID
+# iwpan dev wpan0 set pan_id 0x04D2
+# ip link set wpan0 up
+# ip link set lowpan0 up
+
+# sysctl -w net.ipv6.conf.lowpan0.forwarding=1
+# sysctl -w net.ipv6.conf.lowpan0.accept_ra=1
+
+# ip -6 addr add fd00::$ID/64 dev lowpan0
