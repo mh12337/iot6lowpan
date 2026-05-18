ac8f5d4b6bbcaf370b40fe1ac7e615f73fa4258d
morte <202109169@post.au.dk>
2026-05-18 18:58:32 +0200

    update metric and analyzer scripts

--- lowpan/hops/get_iperf_metrics.py
+++ lowpan/hops/get_iperf_metrics.py
@@ -15,16 +15,17 @@ import shlex
 import subprocess
 from pathlib import Path
 import time
+import sys
 
-
-BITRATES_KBPS = [10, 40, 80]
-PAYLOAD_SIZES = [64, 128, 256, 512]
+BITRATES_KBPS = [10, 30, 50, 60, 70, 80]
+PAYLOAD_SIZES = [64, 96, 128, 256, 512, 1024]
 INTERFACE = "lowpan0"
 PORT = 5123
-OUTPUT_DIR = "."
+OUTPUT_DIR = "./results"
 IPERF_BIN = "iperf3"
+TEST_TIME = 30
 
-MAX_RETRIES = 3
+MAX_RETRIES = sys.maxsize
 RETRY_DELAY_SEC = 2
 
 
@@ -33,22 +34,24 @@ def run_iperf_test(
     bitrate_kbps: int,
     payload_size: int,
     outdir: Path,
+	tcp: bool = False
 ) -> int:
 
-    output_path = outdir / f"data_b{bitrate_kbps}_l{payload_size}.json"
+    output_path = outdir / f"data_b{bitrate_kbps}_l{payload_size}_{'tcp' if tcp else 'udp'}.json"
 
     cmd = (
         f"{shlex.quote(IPERF_BIN)} "
         f"-c {shlex.quote(destination_with_iface)} "
         f"-p {PORT} "
-        "-u "
+	    f"{'-u ' if not tcp else ''}"
+		f"-t {TEST_TIME} "
         f"-b {bitrate_kbps}k "
         f"-l {payload_size} "
         "--get-server-output -J "
         f"> {shlex.quote(str(output_path))}"
     )
 
-    for attempt in range(1, MAX_RETRIES + 1):
+    for attempt in range(1, MAX_RETRIES):
 
         print(
             f"Running test "
@@ -89,7 +92,8 @@ def main() -> int:
 		description="Run iperf3 tests over bitrate/payload matrix and save JSON outputs."
 	)
 	parser.add_argument("destination", help="Destination IPv6 address, e.g. fd00::8")
-
+	parser.add_argument("--tcp", action="store_true", help="include tcp test")
+	
 	args = parser.parse_args()
 
 	destination_with_iface = f"{args.destination}%{INTERFACE}"
@@ -114,6 +118,20 @@ def main() -> int:
 				outdir=outdir,
 			)
 			if rc != 0:
+				failures += 1
+			time.sleep(2)
+		
+	if (args.tcp):
+		for bitrate in BITRATES_KBPS:
+			for payload in PAYLOAD_SIZES:
+				rc = run_iperf_test(
+				destination_with_iface=destination_with_iface,
+				bitrate_kbps=bitrate,
+				payload_size=payload,
+				outdir=outdir,
+				tcp=True
+            )
+			if rc!=0:
 				failures += 1
 			time.sleep(2)
 
