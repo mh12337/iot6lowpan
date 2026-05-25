242bd0251a789e06c130e16d50013bda1beb01ed
Morten Husted <202109169@post.au.dk>
2026-05-25 15:48:16 +0200

    Modify setup_lowpan_node.sh for address and routing
    Updated short address and modified sysctl settings for lowpan0. Added IPv6 route configuration.

--- rpi/lowpan/setup_lowpan_node.sh
+++ rpi/lowpan/setup_lowpan_node.sh
@@ -9,16 +9,17 @@ iwpan phy phy0 set channel 0 24
 ip link set lowpan0 down
 ip link set wpan0 down
 iwpan dev wpan0 set ackreq_default 1
-#comment in to use extended addr
-#iwpan dev wpan0 set short_addr 0x0008
+iwpan dev wpan0 set short_addr 0xf208
 iwpan dev wpan0 set pan_id 0x04D2
 ip link set wpan0 up
 ip link set lowpan0 up
 
-sysctl -w net.ipv6.conf.lowpan0.forwarding=1
-sysctl -w net.ipv6.conf.lowpan0.accept_ra=1
+#sysctl -w net.ipv6.conf.lowpan0.forwarding=1
+sysctl -w net.ipv6.conf.lowpan0.accept_ra=2
 
-#comment in to use extended addr
-#ip -6 addr add fd00::8/64 dev lowpan0
+ip -6 addr add fd00::8/64 dev lowpan0
+ip -6 route add fd00::/64 dev lowpan0
 
 sudo iwpan dev wpan0 associate pan_id 0x04d2 coord 0x0211223344556677
+
+sudo ip -6 route add 2001:db8:1:ffff::/96 via fe80::11:2233:4455:6677 dev lowpan0
