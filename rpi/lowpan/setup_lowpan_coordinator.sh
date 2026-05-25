246c6b611ce4e2259a7df5adc452b8ec49fd4bb1
Morten Husted <202109169@post.au.dk>
2026-05-25 15:46:05 +0200

    Configure short address and RA settings for lowpan
    Set short address for wpan0 and update RA acceptance level.

--- rpi/lowpan/setup_lowpan_coordinator.sh
+++ rpi/lowpan/setup_lowpan_coordinator.sh
@@ -11,13 +11,14 @@ ip link set wpan0 up
 iwpan phy phy0 set channel 0 24
 ip link set wpan0 down
 iwpan dev wpan0 set ackreq_default 1
-#iwpan dev wpan0 set short_addr 0x0002
 iwpan dev wpan0 set pan_id 0x04D2
+iwpan dev wpan0 set short_addr 0xf402
 ip link set wpan0 up
 
 ip link add link wpan0 name lowpan0 type lowpan
 ip link set lowpan0 up
 
 sysctl -w net.ipv6.conf.lowpan0.forwarding=1
-sysctl -w net.ipv6.conf.lowpan0.accept_ra=1
-#ip -6 addr add fd00::2/64 dev lowpan0
+sysctl -w net.ipv6.conf.lowpan0.accept_ra=2
+
+ip -6 addr add fd00::4/64 dev lowpan0
